7f9f0e40936f508b73a0a4f8e6d8c558c9e464cf
Leon Lau <leondotlau@gmail.com>
2019-09-08 12:44:01 +1000

    heroku scheudler test

--- tests/heroku.test.js
+++ tests/heroku.test.js
@@ -1,7 +1,9 @@
-const cycle = function() {
-  setInterval(() => {
-    console.log('Hello from pbp-node heroku.test.js');
-  }, 3000);
-};
+// const cycle = function() {
+//   setInterval(() => {
+//     console.log('Hello from pbp-node heroku.test.js');
+//   }, 3000);
+// };
 
-cycle();
+// cycle();
+
+console.log('scheduler from ./tests/heroku.test.js');
