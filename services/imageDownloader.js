a5515945ecbf65560c0ba3a5a0ffaa752f1a5eb5
Leon <leondotlau@gmail.com>
2019-09-14 10:46:38 +1000

    vars for strings

--- services/imageDownloader.js
+++ services/imageDownloader.js
@@ -5,15 +5,16 @@
 
 */
 
+const fs = require('fs');
 const axios = require('axios');
 const download = require('download-file');
-const fs = require('fs');
 
 const apiPlayersUrl = 'http://data.nba.net/prod/v1/2019/players.json';
 const playerImgUrl =
   'https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/';
-
 const playerImagesFolder = '../images/players/';
+const resultFailFile = '../services/logs/results-fail.txt';
+const resultSuccessFile = '../services/logs/playerIds.json';
 
 (function main() {
   axios
@@ -21,15 +22,12 @@ const playerImagesFolder = '../images/players/';
     .then(response => {
       let players = response.data.league.standard;
 
-      var failFile = fs.createWriteStream('../services/logs/results-fail.txt', {
+      var failFile = fs.createWriteStream(resultFailFile, {
         flags: 'a'
       });
-      var successFile = fs.createWriteStream(
-        '../services/logs/playerIds.json',
-        {
-          flags: 'a'
-        }
-      );
+      var successFile = fs.createWriteStream(resultSuccessFile, {
+        flags: 'a'
+      });
 
       players.forEach(player => {
         let playerPicUrl = `${playerImgUrl}${player.personId}.png`;
@@ -45,6 +43,7 @@ const playerImagesFolder = '../images/players/';
           }
         });
       });
+
       let files = fs.readdirSync(playerImagesFolder);
       let ids = files.map(file => file.replace(/\.[^/.]+$/, ''));
       let idsJson = JSON.stringify(ids);
