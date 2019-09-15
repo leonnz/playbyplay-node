780e87a93500da9d452fc5279bf3867d5fd36adb
Leon <leondotlau@gmail.com>
2019-09-15 10:44:21 +1000

    try catch

--- tests/scheduleTest.js
+++ tests/scheduleTest.js
@@ -1,10 +1,17 @@
 /**
  *    Game Time Schedule
  *
- *    1. At 5.00pm UTC everyday this schedule runs on a Heroku worker process.
+ *    1. At 5.00pm UTC every day this schedule runs on a Heroku worker process.
  *    2. Gets the days first game start time and pass it to the gametime service.
  *    2. The gametime service runs every 24 seconds and checks for active games and populates the play by play data.
  *
+ *    Schedule time equivalents
+ *      - 5pm UTC
+ *      - 1pm EDT (USA)
+ *      - 3am AEDT (Melbourne, following day)
+ *
+ *    The schedule starts at 1pm EDT presuming the NBA API is refreshed at 12pm EDT.
+ *    The schedule relies on the reloadData.js script to have run at 4.40pm UTC.
  */
 
 const fs = require('fs');
@@ -17,21 +24,31 @@ const getScoreboard = require('../data/getScoreboard');
 const prod = { hour: 17, minute: 0, dayOfWeek: new schedule.Range(0, 6) };
 const test = '*/5 * * * * *';
 
-const mainScheduleRule = test; // Main schedule run time, 4.30pm UTC everyday.
-const gameTimeScheduleRule = '*/24 * * * * *'; // Gametime schedule run time, every 24 seconds.
-const jsonStartTimeFile = './data/start_time.json'; // File with the first days game start time.
+const mainScheduleRule = prod;
+const gameTimeScheduleRule = '*/24 * * * * *';
+const jsonStartTimeFile = './data/start_time.json';
 
 const mainSchedule = schedule.scheduleJob({ rule: mainScheduleRule }, () => {
-  const startTimeFile = fs.readFileSync(jsonStartTimeFile);
-  const startTime = JSON.parse(startTimeFile).gameStartTime;
-  console.log(startTime);
-  gameTimeService(startTime);
+  try {
+    const startTimeFile = fs.readFileSync(jsonStartTimeFile);
+
+    if (startTimeFile.byteLength > 0) {
+      const startTime = JSON.parse(startTimeFile).gameStartTime;
+      console.log(startTime);
+      gameTimeService(startTime);
+    }
+  } catch (error) {
+    console.log(
+      'Somehthing went wrong reading the start_time.json file: ' + error
+    );
+  }
 });
 
 /**
  *
  * @param {String} startTime The start time of the game time service.
  */
+
 const gameTimeService = function(startTime) {
   const date = new Date();
   console.log('This indeed did run at: ' + date);
